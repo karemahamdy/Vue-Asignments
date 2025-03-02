ba0274d81457bbcf57aa8d3ac9d7f5686d594e8f
Karema <karemahamdy51@gmail.com>
2025-03-02 14:03:46 +0200

    feat: add header

---
+++ to-do-list/src/components/layout/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <header>
+    <h6>🎀 Pinky Reasources App 🎀</h6>
+ </header>
+  </div>
+</template>
+
+  
+<style scoped>
+header {
+  background-color: pink;
+  text-align: center;
+  padding: 18px;
+
+}
+
+h6 {
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+}
+
+</style>
