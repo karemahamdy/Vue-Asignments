e31ab3645ccd312c685ec153be20e888fb13556e
karemahamdy <karemahamdy51@gmail.com>
2025-03-12 10:40:45 +0200

    feat: add a first store

--- coach-app/src/pages/Coaches/CoachesList.vue
+++ coach-app/src/pages/Coaches/CoachesList.vue
@@ -6,6 +6,22 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>LIST OF COACHES</ul>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li>
+    </ul>
+<h3 v-else>No Couches found</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed : {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+}
+</script>
