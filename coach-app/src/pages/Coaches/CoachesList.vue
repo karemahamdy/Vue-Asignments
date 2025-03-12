673d2a833d9013de5f28c4f58aaf9c61ac8575d5
karemahamdy <karemahamdy51@gmail.com>
2025-03-12 13:48:01 +0200

    feat: add item list

--- coach-app/src/pages/Coaches/CoachesList.vue
+++ coach-app/src/pages/Coaches/CoachesList.vue
@@ -1,4 +1,3 @@
-
 <template>
   <section>FILTER</section>
   <section>
@@ -7,15 +6,20 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li>
+      <CoachItem v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+        :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></CoachItem>
     </ul>
-<h3 v-else>No Couches found</h3>
+    <h3 v-else>No Couches found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
 export default {
-  computed : {
+  components: {
+    CoachItem
+  },
+  computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
     },
@@ -25,3 +29,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
