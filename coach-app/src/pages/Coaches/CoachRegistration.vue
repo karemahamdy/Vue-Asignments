0ffc5b7a700cafc15ba6f1451203ef32e2b43032
Karema <karemahamdy51@gmail.com>
2025-03-14 02:45:13 +0200

    add  form

--- coach-app/src/pages/Coaches/CoachRegistration.vue
+++ coach-app/src/pages/Coaches/CoachRegistration.vue
@@ -1,3 +1,26 @@
 <template>
-  REGISTER
+  <section>
+    <BaseCard>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </BaseCard>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+import BaseCard from '../../components/ui/BaseCard.vue';
+
+export default {
+  components: {
+    CoachForm,
+    BaseCard
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    }
+  }
+};
+</script>
