a67d58578a28277778301392c50f56e845e39362
Karema <karemahamdy51@gmail.com>
2025-03-25 00:20:49 +0200

    feat :  add index

--- coach-app/src/store/index.js
+++ coach-app/src/store/index.js
@@ -1,11 +1,21 @@
 import { createStore } from 'vuex';
-import coachesModules from './modules/coaches/index'
+
+import coachesModule from './modules/coaches/index.js';
 
 const store = createStore({
   modules: {
-    coaches: coachesModules
+    coaches: coachesModule
+  },
+  state() {
+    return {
+      userId: 'c3'
+    };
+  },
+  getters: {
+    userId(state) {
+      return state.userId;
+    }
   }
-})
+});
 
-
-export default store
+export default store;
